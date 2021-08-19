270bd260111df10eac7db860438e330e0c9e3819
Grace Parkes <grace@charitycan.ca>
2021-08-19 12:10:00 -0400

    set state for each input field

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,14 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Container from 'react-bootstrap/Container';
 import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
 import './App.scss';
 
 function App() {
-  const [data, setData] = React.useState(null);
+  const [data, setData] = useState(null);
+  const [location, setLocation] = useState("");
+  const [event, setEvent] = useState("");
+  const [people, setPeople] = useState("");
 
   React.useEffect(() => {
     fetch("/api")
@@ -22,17 +25,17 @@ function App() {
       <Form>
         <Form.Group controlId="formEnterLocation">
           <Form.Label>Location of Memory</Form.Label>
-          <Form.Control type="location" placeholder="Enter Location" />
+          <Form.Control type="location" placeholder="Enter Location" onChange={(e) => {setLocation(e.target.value)} } value={location}/>
         </Form.Group>
 
         <Form.Group controlId="formEnterEvent">
           <Form.Label>Event</Form.Label>
-          <Form.Control type="event" placeholder="Enter Event" />
+          <Form.Control type="event" placeholder="Enter Event" onChange={ (e) => {setEvent(e.target.value)} } value={event}/>
         </Form.Group>
 
         <Form.Group controlId="formEnterPeople">
           <Form.Label>Who Were You With?</Form.Label>
-          <Form.Control type="attendees" placeholder="Enter Persons Name" />
+          <Form.Control type="attendees" placeholder="Enter Persons Name" onChange={ (e) => {setPeople(e.target.value)} } value={people}/>
         </Form.Group>
       </Form>
 
