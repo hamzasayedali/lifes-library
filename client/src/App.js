8c4f8046c3c738875f21c4654f9b0460b4f57c32
Matthew Charters <matthew@charitycan.ca>
2021-08-19 11:32:11 -0400

    Add custom bootstrap styles

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import Container from 'react-bootstrap/Container';
 import Button from 'react-bootstrap/Button';
-import './App.css';
+import './App.scss';
 
 function App() {
   const [data, setData] = React.useState(null);
