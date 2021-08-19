8483aff7a9e5ea1874e8e4d6856c51ead87010fb
Matthew Charters <matthew@charitycan.ca>
2021-08-19 15:30:15 -0400

    Move memories into their own component

--- client/src/App.js
+++ client/src/App.js
@@ -6,14 +6,11 @@ import {
   BrowserRouter as Router,
   Switch,
   Route,
-  Link
 } from 'react-router-dom';
 import Person from './Person';
 import Location from './Location';
+import Memories from './Memories';
 import './App.scss';
-import Card from 'react-bootstrap/Card';
-import Row from 'react-bootstrap/Row';
-import Col from 'react-bootstrap/Col';
 
 function Home() {
   const [data, setData] = useState(null);
@@ -48,8 +45,6 @@ function Home() {
       .then((serverMemories)=>setMemories(serverMemories));
   }, []);
 
-
-
   return (
     <Fragment>
       <p>{!data ? "" : data}</p>
@@ -81,30 +76,7 @@ function Home() {
       <h2>
         Old Memories
       </h2>
-      <Row>
-        {memories.map((memory, index) => {
-          return (
-            <Col key={index} xs={12} sm={4} md={3}>
-              <Card style={{ width: '20rem' }}>
-                <Card.Header>
-                  <Card.Title>{memory.title}</Card.Title>
-                </Card.Header>
-                <Card.Body>
-                    <Card.Subtitle>
-                      <Link to={`/locations/${memory.location}`}>{memory.location}</Link>
-                    </Card.Subtitle>
-                    <Card.Text>
-                      {memory.people.split(',').map(person => (
-                    <span><Link to={`/people/${person}`}>{person}</Link>&nbsp;</span>
-                  ))}<br />
-                      <cite>{memory.date}</cite>
-                    </Card.Text>
-                </Card.Body>
-              </Card>
-            </Col>
-          );
-        })}
-      </Row>
+      <Memories memories={memories} />
     </Fragment>
   );
 }
